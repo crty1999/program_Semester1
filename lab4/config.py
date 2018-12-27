382e4f17e5afcb56b50fc07c4e4a542658064625
crty1999 <45713906+crty1999@users.noreply.github.com>
2018-12-27 13:21:03 +0300

    -

---
+++ lab4/config.py
@@ -0,0 +1,6 @@
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "user_id": 6793187,
+    "access_token": "6ae0fefa6ae0fefa6ae0fefac86a87591966ae06ae0fefa36fdd3798ed90b769eb59c02",
+    "version": "5.92"
+}
