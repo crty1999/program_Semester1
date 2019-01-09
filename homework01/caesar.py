f4a384104daf5244a1947c538505e3f77aa5d1f7
crty1999 <45713906+crty1999@users.noreply.github.com>
2019-01-10 00:53:53 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,67 +1,46 @@
-import random
-
-def encrypt_caesar(plaintext):
+def encrypt_caesar(plaintext: str) -> str:
     """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    n = ord('z') - ord('a') + 1
-    for c in plaintext:
-        if not c.isalpha():
-            ciphertext += c
-            continue
-        isUpper = c.isupper()
-        newC = c.lower()
-        asciiCode = ord(newC) - ord("a")
-        asciiCode += 3
-        asciiCode %= n
-        asciiCode += ord("a")
-        newC = chr(asciiCode)
-        if isUpper:
-            newC = newC.upper()
-        ciphertext += newC
-
+        Encrypts plaintext using a Caesar cipher.
+        >>> encrypt_caesar("PYTHON")
+        'SBWKRQ'
+        >>> encrypt_caesar("python")
+        'sbwkrq'
+        >>> encrypt_caesar("Python3.6")
+        'Sbwkrq3.6'
+        >>> encrypt_caesar("")
+        ''
+        """
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            if not chr((ord(plaintext[i]) + 3)).isalpha():
+                ciphertext += chr(ord(plaintext[i]) - 23)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + 3)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(ciphertext: str) -> str:
     """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    n = ord('z') - ord('a') + 1
-    for c in ciphertext:
-        if not c.isalpha():
-            plaintext += c
-            continue
-        isUpper = c.isupper()
-        newC = c.lower()
-        asciiCode = ord(newC) - ord("a")
-        asciiCode += n-3
-        asciiCode %= n
-        asciiCode += ord("a")
-        newC = chr(asciiCode)
-        if isUpper:
-            newC = newC.upper()
-        plaintext += newC
-
+        Decrypts a ciphertext using a Caesar cipher.
+        >>> decrypt_caesar("SBWKRQ")
+        'PYTHON'
+        >>> decrypt_caesar("sbwkrq")
+        'python'
+        >>> decrypt_caesar("Sbwkrq3.6")
+        'Python3.6'
+        >>> decrypt_caesar("")
+        ''
+        """
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            if not chr((ord(ciphertext[i]) - 3)).isalpha():
+                plaintext += chr(ord(ciphertext[i]) + 23)
+            else:
+                plaintext += chr(ord(ciphertext[i]) - 3)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
-    pass
